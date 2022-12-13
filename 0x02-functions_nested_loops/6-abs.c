00cc6c0c7f70a4a4d617a85e5a113944cdadc69b
primeace7 <osaruonamenjeffery@gmail.com>
2022-12-13 14:26:51 -0800

    Add task 6 code file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _abs - print the absolute value of the number input
+*
+* @n : input (int)
+*
+* Return: absolute value of the input
+*/
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-1 * n);
+}
