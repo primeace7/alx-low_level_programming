4cdf3ae19e006a5e6e892000eddfb7c040f5e938
primeace7 <osaruonamenjeffery@gmail.com>
2022-12-13 10:21:17 -0800

    Add task 4 code file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,31 @@
+#include "main"
+
+/**
+* _isalpha - check for an alphabetic character, upper or lower case
+*
+* @c: input (int)
+*
+* Return: int (0 or 1)
+*/
+
+int _isalpha(int c)
+{
+int start_upper;
+int start_lower;
+int stop_upper;
+int stop_lower;
+
+start_upper = 'A';
+start_lower = 'a';
+stop_lower = 'z';
+stop_upper = 'Z';
+
+while (start_upper <= stop_upper)
+{
+if (start_upper == c || start_lower == c)
+return (1);
+start_lower++;
+start_upper++;
+}
+return (0);
+}
