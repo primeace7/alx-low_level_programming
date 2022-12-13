275fc9fb3d43eb302016480490091065630c8294
primeace7 <osaruonamenjeffery@gmail.com>
2022-12-13 08:46:07 -0800

    Modify task 0 code file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * main - program starting point
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 _putchar('_');
@@ -11,5 +16,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
- return (0);
+return (0);
 }
