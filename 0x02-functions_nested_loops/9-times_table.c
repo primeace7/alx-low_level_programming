e4052753fa9e24ceeeb8027744d52c852717659c
primeace7 <osaruonamenjeffery@gmail.com>
2022-12-14 06:24:56 -0800

    Modify task 9 code file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,20 +15,20 @@ while (count1 <= 9)
 {
 while (count2 <= 9)
 {
-if ((count1 *count2) <= 9)
+if ((count1 * count2) <= 9)
 {
 _putchar((count1 * count2) +'0');
 if (count2 < 9)
 {
 _putchar(',');
 _putchar(' ');
-if (((count2 + 1) *count1) <= 9)
+if (((count2 + 1) * count1) <= 9)
 _putchar(' '); }
 }
 else
 {
-_putchar((count1 *count2) / 10 + '0');
-_putchar((count1 *count2) % 10 + '0');
+_putchar((count1 * count2) / 10 + '0');
+_putchar((count1 * count2) % 10 + '0');
 if (count2 < 9)
 {
 _putchar(',');
