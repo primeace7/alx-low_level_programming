8486bfa365b372f8892dadb0e80f453b74ac2c12
primeace7 <osaruonamenjeffery@gmail.com>
2022-12-14 15:59:18 -0800

    Modify task 104 code file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,8 +15,8 @@ term0 = 0;
 term1 = 1;
 count = 0;
 
-/**
-while (count < 98)
+
+while (count < 9)
 {
 term = term0 + term1;
 term0 = term1;
@@ -24,7 +24,6 @@ term1 = term;
 if (count < 97)
 printf("%.Lf, ", term);
 else
-*/
 printf("%.Lf\n", term);
 count++;
 
