ac169a117379b1721b755dcd154aa25bb2d76252
primeace7 <osaruonamenjeffery@gmail.com>
2022-12-14 08:39:00 -0800

    Add task 101 code file

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#incldue<stdio.h>
+
+/**
+* main - program entry point
+*
+* Return: nothing(void)
+*/
+int main(void)
+{
+int sum, n;
+sum = 0;
+n = 0;
+
+while (n < 1024)
+{
+if ((n % 3 == 0) || (n % 5 == 0))
+sum += n;
+
+n++;
+}
+printf("%\n", sum);
+return (0);
+}
