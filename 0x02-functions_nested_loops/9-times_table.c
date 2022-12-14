40c0fa4aaad6cdc24605c4eba1fbae4a1af07eeb
primeace7 <osaruonamenjeffery@gmail.com>
2022-12-14 03:36:03 -0800

    Modify task 9 code file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,30 +7,35 @@
 */
 void times_table(void)
 {
-int start1, start2, stop1, stop2;
+int start1, start2, stop1, stop2, count1, count2;
 start1 = '0';
 start2 = '0';
 stop1 = '9';
 stop2 = '9';
+count1 = 0;
+count2 = 0;
 
 while (start1 <= stop1)
 {
 while (start2 <= stop2)
 {
-if (start2 * start1 <= '9')
+if (start2 <= '9')
 _putchar(start2);
 else
 {
-_putchar((start1 * start2) / 10);
-_putchar((start1 * start2) % 10);
+_putchar((count1 *count2) / 10);
+_putchar((count1 *count2) % 10);
 }
 if (start2 < '9')
 {
 _putchar(',');
 _putchar(' ');
 }
+count2++;
 start2++;
 }
+count2 = 0;
+count1++;
 start1++;
 start2 = '0';
 _putchar('\n');
