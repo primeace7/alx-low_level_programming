03f6502bdae6484123c776465955177f72924eea
primeace7 <osaruonamenjeffery@gmail.com>
2022-12-14 09:04:52 -0800

    Add task 102 code file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+* main - program starting point
+*
+* Return: always 0(success)
+*/
+int main(void)
+{
+int term0, term1, term, n;
+term0 = 1;
+term1 = 2;
+n = 0;
+
+while (n < 48)
+{
+term = term0 + term1;
+term0 = term1;
+term1 = term;
+n++;
+printf("%d, ", term);
+
+}
+retrun (0); }
