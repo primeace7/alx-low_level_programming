8c78c11d08f70a2f9f19f038b194164d33330d3e
primeace7 <osaruonamenjeffery@gmail.com>
2022-12-14 07:22:30 -0800

    Modify task 11 code file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+* print_to_98 - print every successive numer from the input to 98
+*
+* @n: int, the input from which to print until 98
+*
+*/
+void print_to_98(int n)
+{
+
+if (n >= 98)
+{
+while (n >= 98)
+{
+printf("%d", n);
+if (n != 98)
+printf(", ");
+n--;
+}
+}
+
+else
+{
+while (n < 98)
+{
+printf("%d", n);
+if (n != 98)
+printf(", ");
+n++
+}
+}
+}
