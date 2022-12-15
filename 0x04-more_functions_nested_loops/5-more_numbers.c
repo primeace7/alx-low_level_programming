fb3d691081e185506cbef0e76c06f26498248126
primeace7 <osaruonamenjeffery@gmail.com>
2022-12-15 10:38:44 -0800

    Modify task 5 code file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,8 +9,11 @@
 
 void more_numbers(void)
 {
-int count, i;
+int count, i, j;
+j = 10;
 
+while (j)
+{
 for (count = 0; count < 14; count++)
 {
 i = count;
@@ -21,6 +24,8 @@ if (i > 9)
 i %= 10;
 
 _putchar(i + '0');
+}
+j--;
 }
 _putchar('\n');
 }
