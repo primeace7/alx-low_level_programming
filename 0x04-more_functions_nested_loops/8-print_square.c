de193496811a8beeab64bbbfc1420dcaed1264c4
primeace7 <osaruonamenjeffery@gmail.com>
2022-12-15 13:01:38 -0800

    Add task 8 code file

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,13 +12,13 @@ void print_square(int size)
 {
 int i, j;
 
-for (i = 0; i <= size; i++)
-{
-for (j = 0; j <= size; j++)
-{
-if (size == 0)
+if (size <= 0)
 _putchar('\n');
-else
+
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size; j++)
+{
 _putchar('#');
 }
 _putchar('\n'); }
