5c11d819360996f4908f9dbc0f69291d2d234203
primeace7 <osaruonamenjeffery@gmail.com>
2022-12-15 13:20:17 -0800

    Modify task 9 code file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,11 +16,16 @@ for (i = 1; i <= 100; i++)
 if (i % 15 == 0)
 printf("FizzBuzz ");
 else if (i % 5 == 0)
+{
+if (i == 100)
+printf("Buzz");
+else
 printf("Buzz ");
+}
 else if (i % 3 == 0)
 printf("Fizz ");
 else
-printf("%d", i);
+printf("%d ", i);
 }
 printf("\n");
 return (0);
