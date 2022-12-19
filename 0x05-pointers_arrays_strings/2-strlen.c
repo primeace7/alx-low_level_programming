b90a25aaa6a21861b5f0ee4e2a3360c0e8ecdc06
primeace7 <osaruonamenjeffery@gmail.com>
2022-12-19 12:37:23 -0800

    Modify task 2 code file

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,13 +10,13 @@ int _strlen(char *s)
 {
 	int i, count;
 
-	i = s[i];
+	i = 0;
+	count = 0;
 
-	while (i != '\0')
+	while (s[i] != '\0')
 	{
 		++count;
 		++i;
-		i = s[i];
 	}
 	return (count);
 
