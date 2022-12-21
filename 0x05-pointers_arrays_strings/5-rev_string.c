3a19860fb6fb1a3ba96f793e2409146ecef5b09d
primeace7 <osaruonamenjeffery@gmail.com>
2022-12-21 08:44:28 -0800

    Add task 5 code file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse an input string
+ *
+ * @s: a pointer to the string to be reversed
+ *Return: nothing (void)
+ */
+
+
+void rev_string(char *s)
+{
+	int i, count, j;
+
+	count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+
+	i = count / 2;
+
+
+	for (j = 0; j <= i; j++)
+	{
+		s[j] = s[count - j];
+	}
+}
