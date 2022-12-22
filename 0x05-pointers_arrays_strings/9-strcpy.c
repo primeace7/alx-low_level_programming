93eeaeb0ac47a18a79c7ed05b160bed3fdf09fd9
primeace7 <osaruonamenjeffery@gmail.com>
2022-12-22 04:20:36 -0800

    Add task 9 code file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy a string from src to dest
+ * @src: a pointer to the source of the string to copy
+ * @dest: a pointer to the destination to copy to
+ * Return: a pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	return (char *dest);
+}
