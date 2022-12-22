89a59fd102627798195e0ec9c0c238a31e61bc2d
primeace7 <osaruonamenjeffery@gmail.com>
2022-12-22 04:04:23 -0800

    Modify task 8 code file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -13,7 +14,6 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-
 		if (i != (n - 1))
 			printf("%d, ", a[i]);
 		else
