9504cc3eb8ef616399201a305dac45d8ab79829a
primeace7 <osaruonamenjeffery@gmail.com>
2022-12-21 16:24:29 -0800

    Modify task 6 code file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,14 +10,18 @@
 void puts2(char *str)
 {
 	int i;
+
 	i = 0;
 
-	while (str[i] != '\0')
+	if (str[i] != 0)
 	{
-		if (i == 0)
-			_putchar(str[i]);
-		else if (str[i] != '\0' && str[i-1] != '\0')
-			_putchar(str[i]);
+		_putchar(str[i]);
+		i++;
+	}
+
+	while (str[i] != '\0' && str[i - 1] != '\0')
+	{
+		_putchar(str[i]);
 		i += 2;
 	}
 	_putchar('\n');
