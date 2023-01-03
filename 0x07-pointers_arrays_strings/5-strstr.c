4811ca484a13c78894cc4f1e08d7f0f289803d6a
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-03 04:35:03 +0000

    Add task 5 code file

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - find a substring within another string
+ * @needle: the substring to be found
+ * @haystack: the string to be searched for a substring
+ * Return: a pointer to the of the located substring within the larger string,
+ * or NULL, if the substring is not found
+ */
+
+char *_strstr(char *needle, char *haystack)
+{
+	unsigned int i, j, check;
+
+	check = 1;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (needle[0] == haystack[i])
+			for (j = 1; needle[j] != '\0' && check; j++)
+			{
+				if (needle[j] == haystack[i + j])
+					check = 1;
+				else
+					check = 0;
+			}
+
+		if (needle[j] == '\0')
+			return (&haystack[i]);
+	}
+
+	return (NULL);
+}
