18bb2ae452266e8de98a6e90b436c17e8127b692
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-06 02:22:28 +0000

    Add task 100 code file

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * is_palindrome - determine if a string is a palindrome
+ * @s: pointer to the string to check
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	int len, res, n;
+
+	n = 0;
+
+	len = slen(s, n);
+	res = comp(s, len);
+
+	return (res);
+}
+
+/**
+ * slen - find the length of a string
+ * @s: pointer to the string which length to find
+ * @n: placeholder variable to store the string length
+ * Return: the length of the string
+ */
+
+int slen(char *s, int n)
+{
+	if (*s == '\0')
+		return (n);
+	++n;
+	return (slen((s + 1), n));
+}
+
+/**
+ * comp - compares the characters in a string to know if it's a palindrome
+ * @s: the string to check
+ * @n: the length of the string
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+
+int comp(char *s, int n)
+{
+	if (n == 0 || n == 1)
+		return (1);
+	if (n / 2 && (*s == *s(s + n + 1)))
+		return (comp(s + 1, n - 2));
+	return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,6 @@ int _sqrt_recursion(int n);
 int go(int n, int s);
 int is_prime_number(int n);
 int go(int, int);
+int is_palindrome(char *);
+int slen(char *, int);
+int comp(char *, int);
