6880e7d604ca4e7373719c374a276a4d81293404
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-06 01:44:04 +0000

    Modify task 6 code file to fix compilation error

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -20,12 +20,14 @@ int is_prime_number(int n)
 
 int go(int n, int s)
 {
-	if (n == 2)
-		return (1);
-	if (n < 0)
+	if (n == 1)
+		return (0);
+	if (n < 1)
 		return (0);
 	if (n % s == 0)
 		return (0);
+	if (s == 1)
+		return (1);
 	else
 		return (go(n, s - 1));
 }
