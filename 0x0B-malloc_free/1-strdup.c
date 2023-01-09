e504cd38d0594538ee651b4d75778746dff60e5d
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-09 20:08:59 +0000

    Add task 1 code file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicate an input string in a new memory location on the heap
+ * @str: the string to be duplicated
+ * Return: a pointer to the duplicate string, or NULL if str == NULL, or NULL
+ * if the operation failed
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	s = malloc(slen(str) * sizeof(char) + 1);
+	if (s == NULL)
+		return (NULL);
+	scpy(str, s);
+	return (s);
+}
+
+/**
+ * slen - find the length of a string
+ * @s: pointer to the string which length is to be found
+ * Return: the length of the string
+ */
+
+int slen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + slen(s + 1));
+}
+
+/**
+ * scpy - copy the conent of one string to another
+ * @a: the source string
+ * @b: the destination string
+ * Return: nothing
+ */
+
+void scpy(char *a, char *b)
+{
+	if (*a == '\0')
+	{
+		*b = *a;
+		return;
+	}
+
+	*b = *a;
+	return (scpy(a + 1, b + 1));
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,3 +4,5 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *create_array(unsigned int size, char c);
+int slen(char *s);
+void scpy(char *a, char *b);
