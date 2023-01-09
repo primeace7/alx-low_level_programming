6271421e87fb70ffc4914f9926486a3c3bdea938
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-09 18:52:01 +0000

    Add task 0 code file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <limits.h>
+
+/**
+ * create_array - create an array of chars, and initialize it with
+ * a given character
+ * @size: the size of the array to create
+ * @c: the character to initialize the array with
+ * Return: a pointer to the created array, NULL if size is 0, or NULL if
+ * the array creation failed
+ */
+
+char *creae_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	i = 0;
+
+	while (i++ < size)
+		s[i] = c;
+
+	return (s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,6 @@
+This directory contains code files for the **C - malloc, free** project. The project covers the following concepts:
+- Dynamic memory allocation in C
+- C *malloc* funcion
+- C *free* function
+
+There are 5 mandatory tasks and 2 advanced tasks in thi project, making a total of 7 tasks.
