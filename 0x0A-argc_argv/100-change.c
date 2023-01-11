263e997990ac5c2903434f24c66f4e2b8c700a24
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-11 15:04:38 +0000

    Modify task 100 code file to fix compilation errors

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,30 +13,30 @@ int main(int argc, char **argv)
 	int count, change;
 
 	count = 0;
-	change = int(atoi(argv[1]));
+	change = (int)atoi(argv[1]);
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	if (atoi(argv[1]) <= 0)
-		printf("%\n", 0);
-	while (change > 25)
+		printf("%d\n", 0);
+	while (change >= 25)
 	{
 		count += change / 25;
 		change %= 25;
 	}
-	while (change > 10)
+	while (change >= 10)
 	{
 		count += change / 10;
 		change %= 10;
 	}
-	while (change > 5)
+	while (change >= 5)
 	{
 		count += change / 5;
 		change %= 5;
 	}
-	while (change > 2)
+	while (change >= 2)
 	{
 		count += change / 2;
 		change %= 2;
