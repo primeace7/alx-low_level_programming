085b79586462538c6165d3363e90aff8f410c99a
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-13 12:18:13 +0000

    Modify task 2 code file to fix segfault

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 	len1 = slen(s1);
 	len2 = slen(s2);
 	len3 = len1 + len2 + 1;
-	dest = malloc(len3 * sizeof(char));
+	dest = malloc(len3 * sizeof(*dest));
 	if (dest == NULL)
 		return (NULL);
 	s1_null = is_null(s1);
@@ -71,8 +71,8 @@ void scopy(char *s, char *d)
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
-		*d = *s;
-	*d = *s;
+		*d = s[i];
+	*d = s[i];
 }
 
 /**
