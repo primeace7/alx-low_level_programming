a9c4fd8dbd5af8c9ea01ee527fd0c6219e5e4df5
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-13 03:55:26 +0000

    Add task 0 code file

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - allcate memory using malloc and terminate with an
+ * exit status
+ * @b: the number of bytes to allcate in memory
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p != NULL)
+		return (p);
+	else
+		return (exit(98));
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,6 @@
+This directory contains code files for the **More malloc, free** project. The project covers the following concepts:
+- The calloc function
+- The realloc function
+- The exit function
+
+There are 5 mandatory tasks and 2 advanced tasks in this project, making a total of 7 tasks.
