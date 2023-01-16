6ab9ad73726400da1fe2c61409a09f9a42852831
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-16 21:09:52 +0000

    Add task 5 code file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - free the memory allocations of a dog type
+ * @d: a pointer to the dog type to free
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
