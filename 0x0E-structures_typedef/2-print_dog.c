8e9f4fdb153ac8dea20dccb6a137d19df98d7688
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-16 20:33:57 +0000

    Modify task 2 code file to fix compile errors

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,16 +12,13 @@ void print_dog(struct dog *d)
 	char *dogname;
 	char *dogowner;
 
-	dogage = d->age != NULL ? atoid->age : "(nil)";
 	dogname = d->name != NULL ? d->name : "(nil)";
+	dogowner = d->owner != NULL ? d->owner : "(nil)";
 
 	if (d != NULL)
 	{
 		printf("Name: %s\n", dogname);
-		if (d->age != NULL)
-			printf("Age: %f\n", d->age);
-		else
-			printf("Age: %s\n", "(nil)");
-		printf("Owner: %s\n", d->owner);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", dogowner);
 	}
 }
