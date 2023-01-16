ae5d1d3bff48f355c125a26c7ef950d84d62a4dd
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-16 18:39:16 +0000

    Modify task 1 code file to fix test errors

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -12,8 +11,6 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
