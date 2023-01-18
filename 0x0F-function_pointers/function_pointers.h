34aca8952cb0fd7d96a5150033759cb5c94f7d2a
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-18 21:55:32 +0000

    Fix task 0 test error

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,9 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stddef.h>
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
