0b16be3a8b303756596174daedd6d05d8e4f2fa9
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-19 17:35:17 +0000

    Fix task 2 test errors

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,19 +15,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(arg, n);
 
-	if (n > 0)
+	for (count = 0; count < n; count++)
 	{
-		for (count = 0; count < n; count++)
-		{
-			s = va_arg(arg, char *);
-			if (s != NULL)
-				printf("%s", s);
-			else
-				printf("(nil)");
-			if (separator != NULL)
-				printf("%s", separator);
-		}
-		va_end(arg);
+		s = va_arg(arg, char *);
+		if (s != NULL)
+			printf("%s", s);
+		else
+			printf("(nil)");
+		if (separator != NULL)
+			printf("%s", separator);
 	}
+	va_end(arg);
+
 	printf("\n");
 }
