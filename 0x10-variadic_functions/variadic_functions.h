9159e9724a9dbe59e100ecc5089e2745c6e37862
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-19 16:08:48 +0000

    Add include guard for variadic_function.h file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,11 @@
 #ifndef _VARIADIC_FUNCTION_H_
-#define _VARIADIC_FUCNTION_H_
+#define _VARIADIC_FUNCTION_H_
 
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 
