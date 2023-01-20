1d3ed8070cb899d2f06ab04702e8047c92d3c04c
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-20 04:55:34 +0000

    Add task 3 code file

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,42 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - print all arguments in the specified format
+ * @format: the string that specifies the print format of the arguments
+ * Return; nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list arg;
+	char *s;
+
+	va_start(arg, format);
+
+	while (*format)
+	{
+		switch (*format++)
+		{
+		case 'c':
+			printf("%c", va_arg(arg, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(arg, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(arg, double));
+			break;
+		case 's':
+		{
+			s = va_arg(arg, char *);
+			if (s == NULL)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", s);
+			break;
+		}
+		}
+	}
+}
