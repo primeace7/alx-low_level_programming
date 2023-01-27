6a6fe7ee9abbc033294c3bbba8060dc3a6bdb415
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-27 03:08:17 +0000

    Add header files to lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
 int _putchar(char c);
 size_t print_list(const list_t *h);
 
