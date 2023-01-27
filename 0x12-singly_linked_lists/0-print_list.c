66c414ccb25f190007ebfa6c5c9738f8ac195b19
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-27 03:56:52 +0000

    Fix task 0 code file test errors

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,13 +10,12 @@ size_t print_list(const list_t *h)
 {
 	size_t count;
 
-	for (count = 0; h->next != NULL; count++)
+	for (count = 0; h != NULL; count++)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		count++;
 	}
 
 	return (count);
