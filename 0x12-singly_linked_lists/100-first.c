f3bd003bc8b6dd15b8cae26637276dcf4dedec91
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-29 22:03:37 +0000

    Fix task 100 test errors

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,8 +9,8 @@ void __attribute__ ((constructor)) print_before_main(void)
 {
 	char *st1, *st2;
 
-	st1 = "You're beat! and yet, you must allow,";
-	st2 = "\nI bore my house upon my back!\n";
+	st1 = "You're beat! and yet, you must";
+	st2 = "allow, \nI bore my house upon my back!\n";
 	strcat(st1, st2);
 	printf("%s", st1);
 }
