8b4971efc1c6a46372f2d242fcc911173edc1e06
primeace7 <osaruonamenjeffery@gmail.com>
2023-01-29 08:12:01 +0000

    Add task 100 code file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * print_before_main - print a string before main executes
+ * Return: none
+ */
+
+void __attribute__ ((constructor)) print_before_main(void)
+{
+	char *st1, *st2;
+
+	st1 = "You're beat! and yet, you must allow,";
+	st2 = "\nI bore my house upon my back!\n";
+	strcat(st1, st2);
+	printf("%s", st1);
+}
