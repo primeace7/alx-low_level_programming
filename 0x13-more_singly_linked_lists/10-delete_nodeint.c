110c75696ea1cd4d03a951e860a3568a1ed53918
primeace7 <osaruonamenjeffery@gmail.com>
2023-02-01 17:19:38 +0000

    Add task 10 code file, fix task 5 test error

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,1 +1,39 @@
 #include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete an element of a linked list
+ * at a given index
+ * @head: pointer to the head pointer of the list
+ * @index: the index of the list to delete
+ * Return: 1 if the deletion succeeds, -1 otherwise
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *hold;
+	unsigned int i;
+
+	temp = *head;
+
+	if (index == 0)
+	{
+		if (temp == NULL)
+			return (-1);
+		*head = temp->next;
+		free(temp);
+	}
+
+	for (i = 0; i < index; i++)
+	{
+		if (temp == NULL)
+			return (-1);
+		hold = temp;
+		temp = temp->next;
+	}
+
+	if (temp == NULL)
+		return (-1);
+	hold->next = temp->next;
+	free(temp);
+	return (1);
+}
