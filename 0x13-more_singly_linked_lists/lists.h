58b9d1b97dca5cb06c52f774091a1f8e0be5bf7a
primeace7 <osaruonamenjeffery@gmail.com>
2023-02-01 13:24:35 +0000

    Fix betty errors for lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,10 +1,18 @@
 #ifndef _MORE_SINGLY_LINKED_LISTS_
 #define _MORE_SINGLY_LINKED_LISTS_
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 #include <stdio.h>
