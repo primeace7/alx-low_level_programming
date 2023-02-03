dde8ac215e40d60b836f3aa6ef28da35c85251e7
primeace7 <osaruonamenjeffery@gmail.com>
2023-02-03 13:53:55 +0000

    Add task 5 code file

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - find the number of bytes to flip, to make the
+ * binary word of one number equal to another
+ * @n: the first number
+ * @m: the second number
+ * Return: the number of bits to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int count, result;
+
+	result = m ^ n;
+
+	while (result > 0)
+	{
+		count += result & 1;
+		result >>= 1;
+	}
+
+	return (count);
+}
