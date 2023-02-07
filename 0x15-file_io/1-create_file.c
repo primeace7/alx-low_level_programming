74c1aa5cbb2453505e14792432062dcea56ea4f8
primeace7 <osaruonamenjeffery@gmail.com>
2023-02-07 04:59:04 +0000

    Add task 1 code file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: the name of the file to create
+ * @text_content: pointer to the content of the file to create
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, write_out;
+
+	if (filename == NULL)
+		return (-1);
+	fd  open(filename, O_WRONLY|O_CREAT|O_TRUNC, 600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (*text_content++ != '\0')
+		{
+			write_out = write(fd, text_content, 1);
+			if (write_out == -1)
+				rturn (-1);
+		}
+		write_out = write(fd, text_content, 1);
+	}
+	if (write_out == -1)
+		return (-1);
+	return (1);
+}
