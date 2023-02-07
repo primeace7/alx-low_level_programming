f4fc38689f560cdfa6ee24d673a58d20798ec8c6
primeace7 <osaruonamenjeffery@gmail.com>
2023-02-07 04:50:16 +0000

    Add task 0 code file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * read_textfile - read text from a file and print to POXSIX stdout
+ * @filename: the name of the file to read from
+ * @letters: the number of letters to read and print
+ * Return: the number of letters read and printed, or 0 if none
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char buf[1];
+	int fd, out;
+	size_t i;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+
+	for (i = 0; i < letters; i++)
+	{
+		out = read(fd, buf, 1);
+		if (out == -1)
+			return (0);
+		write(STDOUT_FILENO, buf, 1);
+		out = close(fd);
+		if (out == -1)
+			return (0);
+	}
+	return (i + 1);
+}
