6ed5c581f0b2fdf4544b753afc501a0803f94aa6
primeace7 <osaruonamenjeffery@gmail.com>
2023-02-08 10:34:06 +0000

    Fix task 0 test errors

--- 0x15-file_io/0-read_textfile2.c
+++ 0x15-file_io/0-read_textfile2.c
@@ -9,35 +9,31 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, out, hold;
+	char buf[2];
+	int fd, out;
 	size_t i;
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd == -1)
 		return (0);
 
 	for (i = 0; i < letters; i++)
 	{
-		out = read(fd, &hold, 1);
+		out = read(fd, buf, 1);
 		if (out == -1)
 			return (0);
-		if (out == 0)
-			break;
-		out = write(STDOUT_FILENO, &hold, 1);
+		out = write(STDOUT_FILENO, buf, 1);
 		if (out == -1)
 			return (0);
 	}
-	hold = '\0';
-	out = write(STDOUT_FILENO, &hold, 1);
+	buf[0] = '\0';
+	out = write(STDOUT_FILENO, buf, 1);
 	if (out == -1)
 		return (0);
 	out = close(fd);
 	if (out == -1)
 		return (0);
-	if (i + 1 == letters)
-		return (letters);
-	else
-		return (i + 1);
+	return (i + 1);
 }
