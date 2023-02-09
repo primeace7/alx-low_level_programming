9233c6194f0e3ca8ae4e7949f87e739a23d7f85f
primeace7 <osaruonamenjeffery@gmail.com>
2023-02-09 16:28:02 +0000

    Fix task 1 test errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_TRUNC);
 	if (errno == ENOENT || errno == EACCES)
 	{
-		fd = open(filename, O_WRONLY | O_CREAT, 600);
+		fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 600);
 		if (fd == -1)
 			return (-1);
 	}
