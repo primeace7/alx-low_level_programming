8214448e034bf9cb281c9481c8453b633950b50b
primeace7 <osaruonamenjeffery@gmail.com>
2023-02-09 09:34:18 +0000

    Fix task 2 test errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,23 +13,18 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
 
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		return (-1);
-
-	while (*text_content++ != '\0')
+	if (text_content != NULL)
 	{
-		write_out = write(fd, text_content, 1);
+		write_out = write(fd, text_content, strlen(text_content));
 
 		if (write_out == -1)
 			return (-1);
 	}
-	write_out = write(fd, text_content, 1);
-	if (write_out == -1)
-		return (-1);
+
 	return (1);
 }
