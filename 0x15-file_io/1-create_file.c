bb92cdfaa9dfc4ee259c7b84ea12b1703edca6ce
primeace7 <osaruonamenjeffery@gmail.com>
2023-02-09 09:04:22 +0000

    Fix task 1 test errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,17 +10,14 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, writer;
-	char *copy;
-
-	copy = (char *)filename;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(copy, O_WRONLY | O_TRUNC);
+	fd = open(filename, O_RDWR | O_TRUNC);
 	if (fd == -1)
 	{
-		fd = open(copy, O_WRONLY | O_CREAT, 600);
+		fd = open(filename, O_RDWR | O_CREAT, 600);
 		if (fd == -1)
 			return (-1);
 	}
