1dd046f77c12edae3bc522d81a1e7a30aa622467
primeace7 <osaruonamenjeffery@gmail.com>
2023-02-09 16:02:31 +0000

    Fix task 1 errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,13 +14,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_TRUNC);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 600);
 	if (fd == -1)
-	{
-		fd = open(filename, O_RDWR | O_CREAT, 600);
-		if (fd == -1)
-			return (-1);
-	}
+		return (-1);
 
 	if (text_content != NULL)
 	{
@@ -29,7 +25,9 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 	}
 
-	close(fd);
+	writer = close(fd); /*I'm using writer again instead of a new variable*/
+	if (writer == -1)
+		return (-1);
 
 	return (1);
 }
