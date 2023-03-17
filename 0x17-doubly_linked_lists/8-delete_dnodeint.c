5706bed4bbcfa5d1f2ea4df9e00b90c61c00c62a
primeace7 <osaruonamenjeffery@gmail.com>
2023-03-17 20:03:32 +0000

    Rewrite task 8 code file to delete the right node when index = 0 and return the right result when index is out of range

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -5,35 +5,44 @@
  * list, at a given index
  * @head: pointer to the head pointer of the list
  * @index: the index (zero-based) of the node to delete
+ * Return: 1 if successful, -1 otherwise
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int count;
-	dlistint_t *list, *iter, *new;
+	dlistint_t *list, *hold;
 
 	list = *head;
 
 	if (list == NULL)
 		return (-1);
+	if (index == 0)
+	{
+		if (list->next == NULL)
+			*head = NULL;
+		else
+		{
+			*head = list->next;
+			list->next->prev = NULL;
+		}
+		free(list);
+		return (1);
+	}
 
 	for (count = 0; count < index; count++)
 	{
-		iter = list;
+		hold = list;
 		list = list->next;
+		if (list == NULL)
+			return (-1);
 	}
 
-	if (list == NULL && count < index)
-		return (-1);
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (-1);
+	if (list->next != NULL)
+		list->next->prev = hold;
+	else
+		hold->next = list->next;
 
-	iter->next = new;
-	new->prev = iter;
-	new->next = list->next;
-	list->next->prev = new;
 	free(list);
-
 	return (1);
 }
