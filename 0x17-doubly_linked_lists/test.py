e5fe18a866370924ed9027797f05881075ab62b0
primeace7 <osaruonamenjeffery@gmail.com>
2023-03-18 20:28:16 +0000

    Recalculate the answer for task 102

--- 0x17-doubly_linked_lists/test.py
+++ 0x17-doubly_linked_lists/test.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-a, b = 999, 999
-while b > 0:
-     product = a * b
-     prod_str = str(product)
-     mid = len(prod_str)//2
-     check = True
-     i = 0
-     while check and i <= mid:
-         if prod_str[i] == prod_str[len(prod_str) - 1 - i]:
-                     i += 1
-         else:
-              check = False
-              break
+
+for a in range(999, 99, -1):
+     for b in range(999, 99, -1):
+          product = a * b
+          prod_str = str(product)
+          mid = len(prod_str)//2
+          check = True
+          i = 0
+          while check and i <= mid:
+               if prod_str[i] == prod_str[len(prod_str) - 1 - i]:
+                    i += 1
+               else:
+                    check = False
+                    break
+          if check:
+                    print(f'required num: {b}, {a} * {b} = {a*b}')
+                    break
      if check:
-             print(f'required num: {b}, {a} * {b} = {a*b}')
-             break
-     b -= 1
+          break
 if not check:
      print('There\'s no such number')
