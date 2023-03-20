3af2ea07149df310acd1dfa335f423583628914e
primeace7 <osaruonamenjeffery@gmail.com>
2023-03-20 15:51:00 +0000

    Add task 2 C code file and shared object (100-operations.so)

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - calculate the sum of two numbers
+ * @a: the addend
+ * @b: the augend
+ * Return: the sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - calculate the difference of two numbers
+ * @a: the minuend
+ * @b: the subtrahend
+ * Return: the difference of a and b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul = calculate the product of a and b
+ * @a: the multiplicand
+ * @b: the multiplier
+ * Return: the product of a nad b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - calculate the quotient of a and b
+ * @a: the dividend
+ * @b: the divisor
+ * Return: the quotient of a/b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - calculate the remainder from dividing a by b
+ * @a: the dividend
+ * @b: the divisor
+ * Return: a % b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
