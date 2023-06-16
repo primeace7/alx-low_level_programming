f0422ce24d413c634a589df858c5fd6148aac063
primeace7 <osaruonamenjeffery@gmail.com>
2023-06-16 10:17:19 +0000

    Fix task 0 checker errors for hash table project

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,38 +2,32 @@
 
 /**
  * hash_table_create - create a hashtable structure
- *@size: the size of the table to create, or the number
- *of records in the table to create
- *Return: pointer to the newly created hashtable
+ * @size: the size of the table to create, or the number
+ *     of records in the table to create
+ * Return: pointer to the newly created hashtable
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
-	hash_node_t *nodes, *node;
+	hash_table_t *table;
+	hash_node_t *head_node;
 	unsigned long int count;
 
-	new_table = malloc(sizeof(hash_table_t));
+	head_node = malloc(sizeof(hash_node_t) * size);
+	table = malloc(sizeof(hash_table_t));
 
-	if (new_table == NULL)
+	if (head_node == NULL || table == NULL)
 		return (NULL);
 
-	node = malloc(sizeof(hash_node_t));
-	nodes = malloc(sizeof(hash_node_t) * size);
-
-	if (nodes == NULL || node == NULL)
-		return (NULL);
-
-	node = nodes;
-	new_table->array = &node;
-	new_table->size = size;
+	table->array = &head_node;
+	table->size = size;
 
 	for (count = 0; count < size; count++)
 	{
-		((new_table->array)[count])->key = NULL;
-		((new_table->array)[count])->value = NULL;
-		((new_table->array)[count])->next = NULL;
+		((table->array)[count])->key = NULL;
+		((table->array)[count])->value = NULL;
+		((table->array)[count])->next = NULL;
 	}
 
-	return (new_table);
+	return (table);
 }
